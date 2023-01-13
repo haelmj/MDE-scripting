61a8d5e6ce4ec1a0474cd729b2062ef088a62bfe
haelmj <manj19.mjn@gmail.com>
2023-01-14 00:54:00 +0100

    Adding incomplete file

--- avscan.py
+++ avscan.py
@@ -1,56 +1,73 @@
 import json
 import urllib.request
 import urllib.parse
+import csv
+import requests
+from assets.popups import *
 
-tenantId = '00000000-0000-0000-0000-000000000000' # Paste your own tenant ID here
-appId = '11111111-1111-1111-1111-111111111111' # Paste your own app ID here
-appSecret = '22222222-2222-2222-2222-222222222222' # Paste your own app secret here
+tenantId = '' # Paste your own tenant ID here
+appId = '' # Paste your own app ID here
+appSecret = '' # Paste your own app secret here
+
+
+
+# create prompt to receive this info. Store tenantId and appId on inital config then later on appSecret and csv will always be requested
+# if info exists then move on to popup for appsecret then csv file
+# else show popup to add this info
 
 # get access token
 
-url = "https://login.microsoftonline.com/%s/oauth2/token" % (tenantId)
+class AVScan():
+    def __init__(self):
+        self.tenantId = popup('TenantId', "Enter TenantId")
+        self.appId = appId
+        self.appSecret = appSecret
 
-resourceAppIdUri = 'https://api.securitycenter.microsoft.com'
+    def get_access_token(self):
+        url = f"https://login.microsoftonline.com/{self.tenantId}/oauth2/token"
 
-body = {
-    'resource' : resourceAppIdUri,
-    'client_id' : appId,
-    'client_secret' : appSecret,
-    'grant_type' : 'client_credentials'
-}
+        resourceAppIdUri = "https://api.securitycenter.microsoft.com"
 
-data = urllib.parse.urlencode(body).encode("utf-8")
+        body = {"resource": resourceAppIdUri,"client_id": appId,"client_secret": appSecret,"grant_type": "client_credentials"}
 
-req = urllib.request.Request(url, data)
-response = urllib.request.urlopen(req)
-jsonResponse = json.loads(response.read())
-aadToken = jsonResponse["access_token"]
-
-# get machine ids
-
-url = "https://api.securitycenter.microsoft.com/api/machines"
-headers = { 
-    'Content-Type' : 'application/json',
-    'Accept' : 'application/json',
-    'Authorization' : "Bearer " + aadToken
-}
+        data = urllib.parse.urlencode(body).encode('utf-8')
+        req = urllib.request.Request(url, data)
+        with urllib.request.urlopen(req) as response:
+            jsonResponse = json.loads(response.read())
+            aadToken = jsonResponse["access_token"]
+        return aadToken
+    def trigger_scan(self):
+        pass
 
 
 
-req = urllib.request.Request(url, headers)
-response = urllib.request.urlopen(req)
-jsonResponse = json.loads(response.read())
-machines = jsonResponse["value"]
+# # get machine ids
 
-# trigger av scans
-body = {
-  "Comment": "Check machine for viruses",
-  "ScanType": "Full"
-}
+# url = "https://api.securitycenter.microsoft.com/api/machines"
+# headers = { 
+#     'Content-Type' : 'application/json',
+#     'Accept' : 'application/json',
+#     'Authorization' : "Bearer " + aadToken
+# }
 
-for m in machines:
-    new_url = url + f"/{m.id}/runAntiVirusScan"
-    data = urllib.parse.urlencode(body).encode("utf-8")
-    req = urllib.request.Request(url, data, headers)
-    jsonResponse = json.loads(response.read())
-    print(jsonResponse)
+
+# req = urllib.request.Request(url, headers=headers)
+# response = urllib.request.urlopen(req)
+# jsonResponse = json.loads(response.read())
+# machines = jsonResponse["value"]
+
+
+# # # trigger av scans
+# body = {"Comment": "Check machine for viruses","ScanType": "Full"}
+
+
+# with open('DeviceID.csv') as f:
+#     csvreader = csv.reader(f)
+#     for row in csvreader:
+#         id = row[0]
+#         new_url = url + f"/{id}/runAntiVirusScan"
+#         response = requests.post(new_url, json=body, headers=headers)
+#         jsonResponse = response.json()
+#         print(jsonResponse)
+
+
