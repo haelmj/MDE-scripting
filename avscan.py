32828725cffc42126b6cb4622c2dc7c869bfa670
Michael Ndon <michael.n@principia-advisory.com>
2023-01-14 22:23:29 +0000

    Initial iteration set

--- avscan.py
+++ avscan.py
@@ -1,73 +1,65 @@
 import json
-import urllib.request
-import urllib.parse
 import csv
 import requests
 from assets.popups import *
-
-tenantId = '' # Paste your own tenant ID here
-appId = '' # Paste your own app ID here
-appSecret = '' # Paste your own app secret here
-
-
+from assets.fileexplorer import *
+import json
 
 # create prompt to receive this info. Store tenantId and appId on inital config then later on appSecret and csv will always be requested
 # if info exists then move on to popup for appsecret then csv file
 # else show popup to add this info
 
-# get access token
-
 class AVScan():
     def __init__(self):
-        self.tenantId = popup('TenantId', "Enter TenantId")
-        self.appId = appId
-        self.appSecret = appSecret
+        try:
+            with open('app.json', 'r') as f:
+                data = json.load(f)
+            if data['tenantID'] and data['appID']:
+                self.tenantId = data['tenantID']
+                self.appId = data['appID']
+            else:
+                self.tenantId = popup('TenantId', "Enter TenantId")
+                self.appId = popup('App ID', 'Enter Application ID')
+        except FileNotFoundError:
+            data ={}
+            self.tenantId = popup('TenantId', "Enter TenantId")
+            self.appId = popup('App ID', 'Enter Application ID')
+            data['tenantID'] = self.tenantId
+            data['appID'] = self.appId
+            with open('app.json', 'w') as f:
+                json.dump(data, f)
+        self.appSecret = popup('App Secret', 'Enter Application Secret')
 
     def get_access_token(self):
         url = f"https://login.microsoftonline.com/{self.tenantId}/oauth2/token"
 
         resourceAppIdUri = "https://api.securitycenter.microsoft.com"
-
-        body = {"resource": resourceAppIdUri,"client_id": appId,"client_secret": appSecret,"grant_type": "client_credentials"}
-
-        data = urllib.parse.urlencode(body).encode('utf-8')
-        req = urllib.request.Request(url, data)
-        with urllib.request.urlopen(req) as response:
-            jsonResponse = json.loads(response.read())
-            aadToken = jsonResponse["access_token"]
+        headers = {"Content-Type":"application/x-www-form-urlencoded"}
+        body = {"resource": resourceAppIdUri,"client_id": self.appId,"client_secret": self.appSecret,"grant_type": "client_credentials"}
+        response = requests.post(url, body, headers)
+        jsonResponse = response.json()
+        aadToken = jsonResponse["access_token"]
         return aadToken
     def trigger_scan(self):
-        pass
+        aadToken = self.get_access_token()            
+        url = "https://api.securitycenter.microsoft.com/api/machines"
+        headers = { 
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json',
+            'Authorization' : "Bearer " + aadToken
+        }
 
+        file = fileExplorer()
+        body = {"Comment": "Check machine for viruses","ScanType": "Full"}
 
+        with open(file) as f:
+            csvreader = csv.reader(f)
+            for row in csvreader:
+                id = row[0]
+                new_url = url + f"/{id}/runAntiVirusScan"
+                response = requests.post(new_url, json=body, headers=headers)
+                jsonResponse = response.json()
+                print(jsonResponse)
+            show_info('Trigger Scans', 'Scans Triggered Successfully!')
 
-# # get machine ids
-
-# url = "https://api.securitycenter.microsoft.com/api/machines"
-# headers = { 
-#     'Content-Type' : 'application/json',
-#     'Accept' : 'application/json',
-#     'Authorization' : "Bearer " + aadToken
-# }
-
-
-# req = urllib.request.Request(url, headers=headers)
-# response = urllib.request.urlopen(req)
-# jsonResponse = json.loads(response.read())
-# machines = jsonResponse["value"]
-
-
-# # # trigger av scans
-# body = {"Comment": "Check machine for viruses","ScanType": "Full"}
-
-
-# with open('DeviceID.csv') as f:
-#     csvreader = csv.reader(f)
-#     for row in csvreader:
-#         id = row[0]
-#         new_url = url + f"/{id}/runAntiVirusScan"
-#         response = requests.post(new_url, json=body, headers=headers)
-#         jsonResponse = response.json()
-#         print(jsonResponse)
-
-
+AVScan().trigger_scan()
